dd671bc5d3f37a890b973da69beb6fb8cc9ffd8b
RakeshKumar-07 <rk157082@gmail.com>
2024-04-07 11:58:02 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // index.js
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
